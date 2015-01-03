f4f7f1545bdac59ea1d985dc2a118e221532ae8d
zach111694 <zach111694@yahoo.com>
2015-01-03 03:36:28 -0500

    Added sub-pages from home page.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/photos', function(req, res) {
+  res.render('photos', { title: 'Photos' });
+});
+
+router.get('/videos', function(req, res) {
+  res.render('videos', { title: 'Videos' });
+});
+
+router.get('/articles', function(req, res) {
+  res.render('articles', { title: 'Articles' });
+});
+
+router.get('/clubs', function(req, res) {
+  res.render('clubs', { title: 'Clubs' });
+});
+
+router.get('/events', function(req, res) {
+  res.render('events', { title: 'Events' });
+});
+
+router.get('/socialmedia', function(req, res) {
+  res.render('socialmedia', { title: 'Social Media' });
+});
+
 module.exports = router;
