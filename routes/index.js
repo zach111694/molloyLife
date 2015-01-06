182335f37c282a2477e753103700521eb9268422
santiago_x06@hotmail.com <santiago_x06@hotmail.com>
2015-01-05 23:52:06 -0500

    Added Navigation Page - SV

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Molloy Life' });
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Molloy Life'});
 });
 
-router.get('/photos', function(req, res) {
-  res.render('photos', { title: 'Photos' });
+router.get('/photos', function (req, res) {
+    res.render('photos', {title: 'Photos'});
 });
 
-router.get('/videos', function(req, res) {
-  res.render('videos', { title: 'Videos' });
+router.get('/videos', function (req, res) {
+    res.render('videos', {title: 'Videos'});
 });
 
-router.get('/articles', function(req, res) {
-  res.render('articles', { title: 'Articles' });
+router.get('/articles', function (req, res) {
+    res.render('articles', {title: 'Articles'});
 });
 
-router.get('/clubs', function(req, res) {
-  res.render('clubs', { title: 'Clubs' });
+router.get('/clubs', function (req, res) {
+    res.render('clubs', {title: 'Clubs'});
 });
 
-router.get('/events', function(req, res) {
-  res.render('events', { title: 'Events' });
+router.get('/events', function (req, res) {
+    res.render('events', {title: 'Events'});
 });
 
-router.get('/socialmedia', function(req, res) {
-  res.render('socialmedia', { title: 'Social Media' });
+router.get('/socialmedia', function (req, res) {
+    res.render('socialmedia', {title: 'Social Media'});
 });
+
+router.get('/navigation', function (req, res) {
+    res.render('navigation', {title: 'Navigation'})
+})
 
 module.exports = router;
