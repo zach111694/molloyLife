b47ce7d8c35f2c138eea29c5b19c39f9fd72b9e1
Yago <santiago_x06@hotmail.com>
2015-05-02 22:23:39 -0400

    Created a configuration file for the Youtube API key.

--- external/youtube_api.js
+++ external/youtube_api.js
@@ -1,54 +1,55 @@
 var request = require("request");
-var NodeCache = require( "node-cache" );
-var youtubeCache = new NodeCache({ stdTTL: 300, checkperiod: 120 });
+var config = require('config');
+var NodeCache = require("node-cache");
+var youtubeCache = new NodeCache({stdTTL: 300, checkperiod: 120});
 
 //todo Keys should not be hardcoded
-var apikey = "AIzaSyD6KohaRaKeIFg24tYKx4YUa7rxt156duE";
+var apikey = config.get("Youtube.apiKey");
 var channelid = "UCYq97D1iaRz5fL1c515MX-g";
 
 var requestString = "https://www.googleapis.com/youtube/v3/search?key=" + apikey +
     "&channelId=" + channelid + "&part=snippet,id&order=date&maxResults=5";
 
 self = module.exports = {
-    getVideos: function(callback){
+    getVideos: function (callback) {
         //var cacheKey = "molloy-youtube";
         //var cached = youtubeCache.get(cacheKey);
         //if(cacheKey in cached) {
         //    var result = cached[cacheKey];
-         //   callback(null, result);
+        //   callback(null, result);
         //} else {
-            console.log(requestString);
-            request(requestString,function(error,response,body){
-                body = JSON.parse(body);
-                if(!error) {
-                    //youtubeCache.set(cacheKey, body);
-                    callback(null, body);
-                } else {
-                    callback(error);
-                }
+        console.log(requestString);
+        request(requestString, function (error, response, body) {
+            body = JSON.parse(body);
+            if (!error) {
+                //youtubeCache.set(cacheKey, body);
+                callback(null, body);
+            } else {
+                callback(error);
+            }
 
-            });
+        });
         //}
 
     },
-    getVideoPage: function(callback,pageToken){
+    getVideoPage: function (callback, pageToken) {
         //var cacheKey = "molloy-youtube";
         //var cached = youtubeCache.get(cacheKey);
         //if(cacheKey in cached) {
         //    var result = cached[cacheKey];
         //    callback(null, result);
         //} else {
-            console.log(requestString + "&pageToken=" + pageToken);
-            request(requestString + "&pageToken=" + pageToken, function(error,response,body){
-                body = JSON.parse(body);
-                if(!error) {
-                    //youtubeCache.set(cacheKey, body);
-                    callback(null, body);
-                } else {
-                    callback(error);
-                }
+        console.log(requestString + "&pageToken=" + pageToken);
+        request(requestString + "&pageToken=" + pageToken, function (error, response, body) {
+            body = JSON.parse(body);
+            if (!error) {
+                //youtubeCache.set(cacheKey, body);
+                callback(null, body);
+            } else {
+                callback(error);
+            }
 
-            });
+        });
         //}
 
     }
